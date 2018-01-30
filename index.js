d565147301a89181cfad2201308779d98ad2cc73
Luan Fabiano <luanpab@gmail.com>
2018-01-30 11:10:56 -0200

    small fixes

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import joiErrorParser from './apigee/errors/joiErrorParser'
+import {buildJoiError, buildGenericErrors} from './apigee/errors/joiErrorParser'
 import errorParser from './apigee/errors/errorParser'
 
 export default {
-  buildJoiError: joiErrorParser.buildJoiError,
-  buildGenericErrors: joiErrorParser.buildGenericErrors,
-  errorParse: errorParser
+  buildJoiError,
+  buildGenericErrors,
+  errorParser
 }
