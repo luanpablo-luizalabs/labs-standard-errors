df9af27108e3832024f3a0f6670a780ae5800239
Luan Fabiano <luanpab@gmail.com>
2018-01-31 09:55:42 -0200

    fixed jsonHelpers exports

--- src/helpers/jsonHelpers.js
+++ src/helpers/jsonHelpers.js
@@ -1,21 +1,23 @@
 const _ = require('lodash')
 
-export function clearJson (object) {
+const Helper = () => {}
+
+Helper.clearJson = (object) => {
   if (object instanceof Array) {
     for (var i = object.length - 1; i >= 0; i--) {
-      clearJson(object[i])
-      if (isEmpty(object[i])) {
+      Helper.clearJson(object[i])
+      if (Helper.isEmpty(object[i])) {
         object.splice(i, 1)
       }
     }
   } else if (typeof object === 'object') {
     for (var property in object) {
       var value = object[property]
-      if (isEmpty(value)) {
+      if (Helper.isEmpty(value)) {
         delete object[property]
       } else if (value instanceof Array || typeof value === 'object') {
-        clearJson(value)
-        if (isEmpty(value)) {
+        Helper.clearJson(value)
+        if (Helper.isEmpty(value)) {
           delete object[property]
         }
       }
@@ -23,7 +25,7 @@ export function clearJson (object) {
   }
 }
 
-export function isEmpty (object) {
+Helper.isEmpty = (object) => {
   if (_.isArray(object)) {
     return object.length === 0
   } else if (_.isObject(object)) {
@@ -34,14 +36,16 @@ export function isEmpty (object) {
   return false
 }
 
-export function isString (value) {
+Helper.isString = (value) => {
   return typeof value === 'string'
 }
 
-export function trim (value) {
+Helper.trim = (value) => {
   if (value === undefined || value === null) {
     return ''
   }
   value = String(value)
   return value.replace(/^\s+|\s+$/g, '')
 }
+
+module.exports = Helper
